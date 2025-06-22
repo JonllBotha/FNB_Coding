be0dc7096006fad621c7dfc465a4f04207a03b60
JonllBotha <112683321+JonllBotha@users.noreply.github.com>
2025-06-22 15:22:06 +0200

    Week 7

---
+++ Week 7/hello_world.py
@@ -0,0 +1,129 @@
+# lesson 1, Setting up
+#print("Hello World!")
+
+# lesson 2, strings
+#message = "Hello World!"
+#message2 = """Bob's world
+#is cool"""
+#print(message)
+#print(message2)
+
+# lesson 3, strings pt2
+#message = 'Hello'
+#print(message[0])
+#print(message[1])
+
+#print(message[-1])
+
+#print(len(message))
+
+#message2 = ' Hello, World! '
+
+#print(message2.strip())
+#print(message2.lower())
+#print(message2.split(','))
+#print(message2.upper())
+#print(message2.replace('e', 'a'))
+
+# lesson 4, numeric data
+#print(3)
+
+#num = 3
+#print(type(num))
+
+#num2 = 3.14
+#print(type(num2))
+
+# lesson 5, variables
+#my_variable = 10
+#total_count = 0
+#user = "John"
+
+# lesson 6, operators on numerics
+#x = 10
+#y = 2
+
+#print(x*y)
+#print(x/y)
+#print(x+y)
+#print(x-y)
+#print(x%y)
+#print(x**y)
+
+#x += 2
+#print(x)
+
+# lesson 7, operators on strings
+#str1 = "Hello"
+#str2 = "World"
+
+#print(str1 + " " + str2)
+#print(str1 + " " + str2 + " " + str2)
+#print(str1 * 3)
+
+# lesson 8, control statements
+#num = 0
+#if num > 0:
+    #print("this number is positive")
+#elif num == 0:
+    #print("this number is zero")
+#else:
+    #print("this number is negitive")
+
+# lesson 9, control statements pt2
+#num1 = int(input("please enter the first number: "))
+#num2 = int(input("Please enter the second number: "))
+
+#if num1 > num2:
+    #print(num1, " is greater than ", num2)
+#elif num2 > num1:
+    #print(num2, "is greater than ", num1)
+#else:
+    #print("the numbers are equal")
+
+# lesson 10, loops
+#fruits = ["apple", "banana", "cherry"]
+
+#for fruit in fruits:
+    #print(fruit)
+
+#numbers = [1, 2, 3, 5, 7]
+#for number in numbers:
+    #print(number)
+
+#count = 1
+
+#while count <= 5:
+    #print(count)
+    #count += 1
+
+# lesson 11, loops pt2
+#fruits = ["apple", "banana", "cherry", "date"]
+
+#for fruit in fruits:
+    #if fruit == "cherry":
+        #break
+    #print(fruit)
+
+#print()
+
+#for fruit in fruits:
+    #if fruit == "cherry":
+        #continue
+    #print(fruit)
+
+#print()
+
+#for fruit in fruits:
+    #if fruit == "cherry":
+        #pass
+    #print(fruit)
+
+#count = 0
+
+#while count < 5:
+    #print(count)
+    #count += 1
+    
+    #if count == 3:
+        #break
